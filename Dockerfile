5745a5361941fe6cae2e52f5baaf5c01a384762a
胡朋 <hupeng@duiba.com.cn>
2018-08-30 10:23:19 +0800

    msg

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM hub.c.163.com/library/java:8-alpine
 
 MAINTAINER xiaomo@xiaomo.info
 
-ADD build/libs/*.jar app.jar
+COPY build/libs/*.jar app.jar
 
 EXPOSE 8001
 
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 gradle clean build -x test
 
-docker build -t hub.c.163.com/xiaomoinfo/eurekaserver .
+docker build -t hub.c.163.com/xiaomoinfo/eurekaserver:latest .
 
-docker push hub.c.163.com/xiaomoinfo/eureka
+docker push hub.c.163.com/xiaomoinfo/eurekaserver:latest
